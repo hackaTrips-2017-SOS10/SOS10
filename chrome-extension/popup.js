409fba1b55ae2f97c08be49518efac509c8fc1a4
rydnr <github@acm-sl.org>
2017-01-21 19:17:40 +0100

    WIP

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -14,45 +14,47 @@ d3
 var geoLat;
 var geoLong;
 
+var req;
+var resp;
+
 var geoSuccess = function(position) {
 
     // Do magic with location
     startPos = position;
     geoLat = startPos.coords.latitude;
     geoLong = startPos.coords.longitude;
-  };
 
-//$('#sos10Box').click(function(evt) {
-//    document.close();
-//});
+    req = new XMLHttpRequest();
+
+    req.onreadystatechange = showHotel;
+
+    var hotelName = 'Windsor Luxury';
+    var departureCity = 'Madrid';
+    //var url = 'http://private-744b35-sos10api.apiary-mock.com/tst/';
+    var url = 'https://sos10.azurewebsites.net/api/HttpTriggerJS1?code=q1MoaiMxS/4XaGDAu7DgHm7wNS2cgGm/UQ3HxsyYrDLtclwBjAfcCw==';
+
+    url = url + '&hotelName=';
+    url = url + hotelName;
+    url = url + '&departureCity=';
+    url = url + departureCity;
+    url = url + '&departureLat=';
+    url = url + geoLat;
+    url = url + '&departureLon=';
+    url = url + geoLong;
+
+    //alert('Calling ' + url);
+
+    req.open('GET', url, true);
+    req.send(null);
+};
+
+$('#sos10Box').click(function(evt) {
+    window.close();
+});
 
 navigator.geolocation.getCurrentPosition(geoSuccess, null);
 
-var req = new XMLHttpRequest();
-var resp;
-
-req.onreadystatechange = showHotel;
-
-var hotelName = 'Melia';
-var departureCity = 'Madrid';
-//var url = 'http://private-744b35-sos10api.apiary-mock.com/tst/';
-var url = 'https://sos10.azurewebsites.net/api/HttpTriggerJS1?code=q1MoaiMxS/4XaGDAu7DgHm7wNS2cgGm/UQ3HxsyYrDLtclwBjAfcCw==';
-
-url = url + '&hotelName=';
-url = url + hotelName;
-url = url + '&departureCity=';
-url = url + departureCity;
-url = url + '&departureLat=';
-url = url + geoLat;
-url = url + '&departureLon=';
-url = url + geoLong;
-
-//alert('Calling ' + url);
-
-req.open('GET', url, true);
-req.send(null);
-
-function showHotel() {
+function showHotel(req) {
     if (req.readyState == 4) {
         if (req.status == 200) {
             resp = JSON.parse(req.responseText);
